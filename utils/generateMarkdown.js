9ebc6f8958e9e91835cfa4832fa4f3e8130fad2b
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-12-01 23:28:53 -0800

    added file structure to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,22 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  
-
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  ${data.tableContent}
+  ## Installation
+  ${data.install}
+  ## usage
+  ${data.usage}
+  ## License
+  ${data.license}
+  ## Contributing
+  ${data.contribute}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  ${data.questions}
 `;
 }
 
